7da563ea438158b656548d41eb904f9f46c685d5
ErickRFinger <erick.finger123@gmail.com>
2025-08-20 11:21:57 -0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -121,16 +121,16 @@ function App() {
             <div className="card-sobre-mim">
               <h2>Serviços</h2>
               <ul className="servicos-lista">
-                <li><strong>Formatação Windows 10</strong> — R$ 180,00</li>
-                <li><strong>Formatação Windows 11</strong> — R$ 200,00</li>
-                <li><strong>Clonagem de Windows para outro computador ou disco</strong> — R$ 250,00</li>
-                <li><strong>Montagem de Computador</strong> — R$ 350,00</li>
-                <li><strong>Limpeza de computador + Otimização</strong> — R$ 200,00</li>
-                <li><strong>Otimização completa de sistema</strong> — R$ 50,00</li>
-                <li><strong>Otimização Windows</strong> — R$ 20,00</li>
-                <li><strong>Otimização Windows + CS2</strong> — R$ 35,00</li>
-                <li><strong>Otimização Windows + Redução de Input Lag + CS2</strong> — R$ 50,00</li>
-                <li><strong>Aluguel do aplicativo de Otimização (desenvolvimento próprio)</strong> — R$ 10,00/mês</li>
+                <li><strong>Formatação Windows 10 (PRESENCIAL)</strong> — R$ 180,00</li>
+                <li><strong>Formatação Windows 11(PRESENCIAL)</strong> — R$ 200,00</li>
+                <li><strong>Clonagem de Windows para outro computador ou disco (PRESENCIAL)</strong> — R$ 250,00</li>
+                <li><strong>Montagem de Computador (PRESENCIAL)</strong> — R$ 350,00</li>
+                <li><strong>Limpeza de computador + Otimização (PRESENCIAL)</strong> — R$ 200,00</li>
+                <li><strong>Otimização completa de sistema (REMOTO)</strong> — R$ 50,00</li>
+                <li><strong>Otimização Windows (REMOTO)</strong> — R$ 20,00</li>
+                <li><strong>Otimização Windows + Jogos (REMOTO)</strong> — R$ 35,00</li>
+                <li><strong>Otimização Windows + Redução de Input Lag + Jogos (REMOTO)</strong> — R$ 50,00</li>
+                <li><strong>Aluguel do aplicativo de Otimização (desenvolvimento próprio) (REMOTO)</strong> — R$ 10,00/mês</li>
               </ul>
             </div>
           </section>
@@ -183,4 +183,5 @@ function App() {
 
 
 export default App 
+
 
